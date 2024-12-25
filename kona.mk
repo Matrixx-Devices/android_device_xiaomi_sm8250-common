c1dfaa6fdb0af8864032cb96bfe8e6d78678e71a
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm8250-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- kona.mk
+++ kona.mk
@@ -436,6 +436,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
+    NcmTetheringOverlay \
     WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
