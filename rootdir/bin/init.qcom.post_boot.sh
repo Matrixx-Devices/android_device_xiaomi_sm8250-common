1694a37827c6a8f28ced67d9c9e90ff33f84f6d0
CuriousNom <aryanbiswas7585@gmail.com>
2025-09-07 08:47:57 +0530

    sm8250-common: rootdir: Cleanup schedtune stuff
    * Drop legacy walt and switch to pelt.

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2380,8 +2380,6 @@ case "$target" in
             # cpuset settings
             echo 0-3 > /dev/cpuset/background/cpus
             echo 0-3 > /dev/cpuset/system-background/cpus
-            # choose idle CPU for top app tasks
-            echo 1 > /dev/stune/top-app/schedtune.prefer_idle
 
             # re-enable thermal & BCL core_control now
             echo 1 > /sys/module/msm_thermal/core_control/enabled
@@ -5720,20 +5718,6 @@ case "$target" in
         echo 10 > /sys/devices/platform/msm_sdcc.1/idle_timeout
         ;;
 esac
-
-# Setup runtime schedTune
-echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo "schedutil" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
-echo 1 > /dev/stune/foreground/schedtune.prefer_idle
-echo 0 > /dev/stune/foreground/schedtune.prefer_high_cap
-echo 0 > /dev/stune/foreground/schedtune.boost
-echo 0 > /dev/stune/schedtune.prefer_idle
-echo 0 > /dev/stune/schedtune.prefer_high_cap
-echo 0 > /dev/stune/schedtune.boost
-echo 1 > /dev/stune/top-app/schedtune.prefer_idle
-echo 0 > /dev/stune/top-app/schedtune.prefer_high_cap
-echo 10 > /dev/stune/top-app/schedtune.boost 10
 
 # Post-setup services
 case "$target" in
